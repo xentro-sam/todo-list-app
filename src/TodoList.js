4797ac6be94824981f710def3ffd1f261164daea
Shivam Agarwal <itisshivam22@gmail.com>
2021-11-04 10:28:37 +0530

    Add functionality to remove todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,17 +5,23 @@ import Todo from './Todo';
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.state = { todos: [{ task: "Groom Chickens" }] };
+        this.state = { todos: [] };
         this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
     }
     create(newTodo) {
         this.setState({
             todos: [...this.state.todos, newTodo]
         });
     }
+    remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
+    }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />
+            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />
         });
         return (
             <div>
