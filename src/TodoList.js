c6e2a613704defe7fd9c845702cc83abd8fb1867
Shivam Agarwal <itisshivam22@gmail.com>
2021-11-04 10:53:00 +0530

    Add functionality to edit the todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,7 @@ class TodoList extends Component {
         this.state = { todos: [] };
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
     create(newTodo) {
         this.setState({
@@ -19,9 +20,18 @@ class TodoList extends Component {
             todos: this.state.todos.filter(t => t.id !== id)
         });
     }
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, task: updatedTask }
+            }
+            return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />
+            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} updateTodo={this.update} />
         });
         return (
             <div>
